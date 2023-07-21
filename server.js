cf8f9eb47d678f9630e41643e10f96513e23e268
tanzimtipu <tanzimtipu@gmail.com>
2023-07-21 18:50:23 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,44 @@
-let expess = require('express');
-let app = expess();
+let express = require('express');
+let app = express();
 let port = process.env.port || 3000;
 
-app.use(expess.static(__dirname + '/'));
+app.use(express.static(__dirname + '/'));
 
 app.get('/', (req, res)=>{
     res.render('index.html');
 });
 
 app.get('/addTwoNumbers', (req,res)=>{
-    //grab values from url parameter
+    // grab values from url parameter
     let num1 = req.query.number1;
     let num2 = req.query.number2;
 
-    //do math
+    // Add
     let sum = parseInt(num1) + parseInt(num2);
 
-    //construct response, include: statusCode, message, data
+    // construct response, include: statusCode, message, data
     let obj = {statusCode:200, message: 'success', data:sum}
 
-    //then send back
+    // send back as json object
     res.json(obj)
 });
 
-app.listen(3000, ()=>{
-    //logic to fire server start
-    console.log('server started');
-}); 
+app.get('/multiplyTwoNumbers', (req,res)=>{
+    // grab values from url parameter
+    let num1 = req.query.number1;
+    let num2 = req.query.number2;
+
+    // Multiply
+    let product = parseInt(num1) * parseInt(num2);
+
+    // construct response, include: statusCode, message, data
+    let obj = {statusCode:200, message: 'success', data:product}
+
+    // send back as json object
+    res.json(obj)
+});
+
+app.listen(port, ()=>{
+    // logic to fire server start
+    console.log('Server started on port ' + port);
+});
